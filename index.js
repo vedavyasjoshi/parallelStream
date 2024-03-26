b027b93d7c414b72d34a71e75675b7b061681383
Vedavyas <vedavyasjoshi121@gmail.com>
2024-03-26 18:22:17 +0530

    setup socket.io

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 import http from "http";
 import express from "express";
 import path from "path";
+import { Server as SocketIO } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
+const io = new SocketIO(server);
 
 app.use(express.static(path.resolve("./public")));
+
+io.on("connection", (socket) => {
+  console.log("Socket Connected", socket.id);
+  socket.on('binarystream', stream => {
+    console.log('Binary stream incoming');
+  })
+});
 
 server.listen(3000, () => console.log(`HTTP server is running on port 3000`));
--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ const userVideo = document.getElementById("user-video");
 const startButton = document.getElementById("start-btn");
 
 const state = { media: null };
+const socket = io()
 
 startButton.addEventListener("click", () => {
   const mediaRecorder = new MediaRecorder(state.media, {
@@ -12,6 +13,7 @@ startButton.addEventListener("click", () => {
 
   mediaRecorder.ondataavailable = (ev) => {
     console.log("Binary stream available", ev.data);
+    socket.emit('binarystream', ev.data);
   };
   mediaRecorder.start(25);
 });
